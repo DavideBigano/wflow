08849032c7bc700a3a3701f92e47f606a23522a8
DavideBigano <davide.bigano@gmail.com>
2026-08-04 22:46:28 +0200

    Wire into Shell

--- npm-package/src/tui/components/Shell/Shell.screenshot.test.tsx
+++ npm-package/src/tui/components/Shell/Shell.screenshot.test.tsx
@@ -1,9 +1,14 @@
 import { render } from 'ink-testing-library';
 import { expect, test } from 'vitest';
+import { InputEventProvider } from '../../../lib/inputEventProvider/index.js';
 import { Shell } from './Shell.js';
 
 test('Shell renders the home tab by default', () => {
-    const { lastFrame } = render(<Shell workspaceRoot="/tmp/ws" />);
+    const { lastFrame } = render(
+        <InputEventProvider>
+            <Shell workspaceRoot="/tmp/ws" />
+        </InputEventProvider>,
+    );
 
     expect(lastFrame()).toMatchSnapshot();
 });
